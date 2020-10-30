91604c39b469fe6d3be7b4ee3701cc9f6c736605
Richard Snider <richardnsnider@gmail.com>
2020-10-30 15:38:14 +0000

    add metadata to http response

--- hello_world/run.sh
+++ hello_world/run.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -x
 
-docker build --quiet --tag hello_world_go:latest .
+docker pull registry.hub.docker.com/library/golang:alpine 
+
+GIT_CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
+docker build \
+--build-arg GIT_CURRENT_COMMIT_HASH=${GIT_CURRENT_COMMIT_HASH} \
+--quiet \
+--tag hello_world_go:latest .
+
+export PORT=8080
 
 docker run \
---env PORT=8080 \
--d \
+--env PORT=$PORT \
+--publish $PORT:$PORT \
+--detach \
 --tty \
 --interactive \
 hello_world_go
--- hello_world/source/main.go
+++ hello_world/source/main.go
@@ -5,14 +5,43 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
+var gitCommitHash string
+var processStartTime = time.Now()
+
+type healthCheckData struct {
+	gitCommit        string
+	hostname         string
+	processID        int
+	processStartTime time.Time
+	requestTime      time.Time
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+	hostname, _ := os.Hostname()
+
+	healthCheckData := healthCheckData{
+		gitCommit:        gitCommitHash,
+		hostname:         hostname,
+		processID:        os.Getpid(),
+		processStartTime: processStartTime,
+		requestTime:      time.Now(),
+	}
+
+	// TODO: fix json marshalling
+	// bytes, _ := json.Marshal(healthCheckData)
+	// fmt.Fprintln(w, string(bytes))
+	fmt.Fprintln(w, "hostname: "+healthCheckData.hostname)
+	fmt.Fprintln(w, "git commit: "+healthCheckData.gitCommit)
+	fmt.Fprintln(w, "process id: "+strconv.Itoa(healthCheckData.processID))
+	fmt.Fprintln(w, "process start time: "+healthCheckData.processStartTime.String())
+	fmt.Fprintln(w, "request time: "+healthCheckData.requestTime.String())
 }
 
 func main() {
-	port := ":" + os.Getenv("PORT")
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
