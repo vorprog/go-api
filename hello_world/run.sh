a00a62db78d2f5ff946d90f6fbdcbe3da24131a8
Richard Snider <richardnsnider@gmail.com>
2020-11-03 22:04:29 +0000

    general revamp and add guid request

--- hello_world/run.sh
+++ hello_world/run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -xe
 
-export PORT=8080
+export ENVIRONMENT=${1:-dev}
+export PORT=${2:-8080}
 
 docker run \
---env PORT=$PORT \
---publish $PORT:$PORT \
+--env APP_ENVIRONMENT_CONFIGURATION=$ENVIRONMENT \
+--publish $PORT:8080 \
 --detach \
 --tty \
 --interactive \
-hello_world_go
+hello_world_go --port 8080
--- hello_world/source/main.go
+++ hello_world/source/main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-var appVersion = `1.0`
 var buildDateVersionLinkerFlag string
 var buildCommitLinkerFlag string
+
+var appVersion = `1.0`
 var processStartTime = time.Now()
 
+var guidGeneratorURL = "http://www.uuidgenerator.net/api/version1"
+var defaultPort = "8080"
+var port = flag.String("port", defaultPort, "port to listen on")
+
 type appMetaData struct {
-	Version          string    `json:"Version"`
-	BuildGitCommit   string    `json:"BuildGitCommit"`
-	BuildDateVersion string    `json:"BuildDateVersion"`
-	Hostname         string    `json:"Hostname"`
-	ProcessStartTime time.Time `json:"ProcessStartTime"`
-	CurrentTimestamp time.Time `json:"CurrentTimestamp"`
+	EnvironmentConfiguration string    `json:"EnvironmentConfiguration"`
+	Version                  string    `json:"Version"`
+	BuildGitCommit           string    `json:"BuildGitCommit"`
+	BuildDateVersion         string    `json:"BuildDateVersion"`
+	Hostname                 string    `json:"Hostname"`
+	ProcessStartTime         time.Time `json:"ProcessStartTime"`
+	CurrentTimestamp         time.Time `json:"CurrentTimestamp"`
+	RequestGUID              string    `json:"RequestGUID"`
+}
+
+var hostname, hostnameError = os.Hostname()
+
+var healthCheckMetaData = appMetaData{
+	EnvironmentConfiguration: os.Getenv("APP_ENVIRONMENT_CONFIGURATION"),
+	Version:                  appVersion,
+	BuildGitCommit:           buildCommitLinkerFlag,
+	BuildDateVersion:         buildDateVersionLinkerFlag,
+	Hostname:                 hostname,
+	ProcessStartTime:         processStartTime,
 }
 
 func rootPathHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,31 +49,40 @@ func rootPathHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func healthCheckHander(w http.ResponseWriter, r *http.Request) {
-	hostname, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err)
+	healthCheckMetaData.CurrentTimestamp = time.Now()
+	healthCheckMetaData.RequestGUID = getURL(guidGeneratorURL)
+	metaDataBytes, _ := json.MarshalIndent(healthCheckMetaData, "", "  ")
+	fmt.Fprintln(w, string(metaDataBytes))
+}
+
+func getURL(url string) string {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	response, httpGetError := http.Get(url)
+	if httpGetError != nil {
+		log.Fatal(httpGetError)
 	}
 
-	healthCheckMetaData := appMetaData{
-		Version:          appVersion,
-		BuildGitCommit:   buildCommitLinkerFlag,
-		BuildDateVersion: buildDateVersionLinkerFlag,
-		Hostname:         hostname,
-		ProcessStartTime: processStartTime,
-		CurrentTimestamp: time.Now(),
+	defer response.Body.Close()
+	body, responseBodyError := ioutil.ReadAll(response.Body)
+	if responseBodyError != nil {
+		log.Fatal(responseBodyError)
 	}
 
-	bytes, _ := json.MarshalIndent(healthCheckMetaData, "", "  ")
-	fmt.Fprintln(w, string(bytes))
+	return string(body)
 }
 
 func main() {
+	if hostnameError != nil {
+		log.Fatal(hostnameError)
+	}
+
 	http.HandleFunc("/", rootPathHandler)
 	http.HandleFunc("/healthcheck", healthCheckHander)
 
-	port := ":" + os.Getenv("PORT")
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		log.Fatal(err)
+	portSpecification := ":" + *port
+	serverError := http.ListenAndServe(portSpecification, nil)
+	if serverError != nil {
+		log.Fatal(serverError)
 	}
 }
