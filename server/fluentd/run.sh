5bcc1e1b411b5a8b1f7fb88663468b6f63b16a58
Richard Snider <richardnsnider@gmail.com>
2021-07-23 00:37:25 +0000

    set environment variables for fluentd

--- server/fluentd/run.sh
+++ server/fluentd/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+S3_BUCKET_NAME=${1:-vorprog-logs}
+SERVICE_NAME=${2:-go-server}
+
+export META_DATA_URL="http://169.254.169.254/latest/meta-data"
+export INSTANCE_ID=$(curl $META_DATA_URL/instance-id)
+export LOCAL_IP=$(curl $META_DATA_URL/local-ipv4) # TODO: Where to use this?
+export PUBLIC_IP=$(curl $META_DATA_URL/public-ipv4) # TODO: Where to use this?
+export AVAILABILITY_ZONE=$(curl $META_DATA_URL/placement/availability-zone)
+export AWS_REGION=${AVAILABILITY_ZONE::-1}
+
 docker run -d \
 --env AWS_REGION=$AWS_REGION \
 --env S3_BUCKET_NAME=$S3_BUCKET_NAME \
