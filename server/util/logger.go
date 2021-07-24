a73e932ea61fd0fb1ab351436175af9cddf4d668
Richard Snider <richardnsnider@gmail.com>
2021-07-24 00:23:07 +0000

    fix logger to keep objects as single line json

--- server/util/logger.go
+++ server/util/logger.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"runtime"
@@ -11,8 +12,14 @@ func Log(message ...interface{}) {
 	log.SetFlags(0)
 	_, file, line, ok := runtime.Caller(1)
 	if ok {
+		var messageString string
+		jsonMessage, jsonMarshalError := json.Marshal(message)
+		if jsonMarshalError != nil {
+			messageString = fmt.Sprint(jsonMarshalError)
+		} else {
+			messageString = string(jsonMessage)
+		}
 		callerPrefix := file + "(" + strconv.Itoa(line) + ") "
-		messageString := fmt.Sprint(message...)
 		log.Println(callerPrefix + messageString)
 	}
 }
