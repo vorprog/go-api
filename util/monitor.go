363fe14aa00063fa2f381dd10ab8be8c01d7c183
Richard Snider <richardnsnider@gmail.com>
2021-08-04 04:58:16 +0000

    simplify monitoring

--- util/monitor.go
+++ util/monitor.go
@@ -1,50 +1,21 @@
 package util
 
 import (
-	"fmt"
-	"io/ioutil"
-	"strconv"
-	"strings"
+	"runtime"
 	"time"
 )
-
-func getCPUSample() (idle, total uint64) {
-	contents, err := ioutil.ReadFile("/proc/stat")
-	if err != nil {
-		return
-	}
-	lines := strings.Split(string(contents), "\n")
-	for _, line := range lines {
-		fields := strings.Fields(line)
-		if fields[0] == "cpu" {
-			numFields := len(fields)
-			for i := 1; i < numFields; i++ {
-				val, err := strconv.ParseUint(fields[i], 10, 64)
-				if err != nil {
-					fmt.Println("Error: ", i, fields[i], err)
-				}
-				total += val // tally up all the numbers to get total ticks
-				if i == 4 {  // idle is the 5th field in the cpu line
-					idle = val
-				}
-			}
-			return
-		}
-	}
-	return
-}
 
 func Monitor() {
 	for {
 		time.Sleep(2 * time.Second)
-		idle0, total0 := getCPUSample()
-		time.Sleep(2 * time.Second)
-		idle1, total1 := getCPUSample()
 
-		idleTicks := float64(idle1 - idle0)
-		totalTicks := float64(total1 - total0)
-		cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+		memory := &runtime.MemStats{}
+		runtime.ReadMemStats(memory)
 
-		Log("CPU usage is " + fmt.Sprint(cpuUsage*100) + "%")
+		Log(map[string]interface{}{
+			"cpu":       runtime.NumCPU(),
+			"memory":    memory.Alloc,
+			"goRoutine": runtime.NumGoroutine(),
+		})
 	}
 }
