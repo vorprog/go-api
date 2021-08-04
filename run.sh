56c8f478fa3fe21f12556c9249bae7deb7fd5fdc
Richard Snider <richardnsnider@gmail.com>
2021-08-04 02:48:25 +0000

    update run script to be match workflow

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ export COMMIT_SHA_TAG=$(git rev-parse HEAD)
 export APP_ENVIRONMENT_CONFIGURATION=${1:-dev}
 export HOST_PORT=${2:-80}
 export APP_SERVER_PORT=8080
+export FLUENTD_HOST="fluentd.vorprog.com:24224"
 
 sudo docker build \
 --build-arg BUILD_COMMIT=$COMMIT_SHA_TAG \
@@ -15,8 +16,9 @@ sudo docker build \
 
 sudo docker run \
 --log-driver=fluentd \
---log-opt tag="{{.ImageName}}/{{.ImageID}}/{{.ID}}" \
+--log-opt fluentd-address=$FLUENTD_HOST \
 --log-opt fluentd-sub-second-precision=true \
+--log-opt tag="docker.{{.ImageName}}.{{.ImageID}}.{{.ID}}" \
 --env APP_ENVIRONMENT_CONFIGURATION=$APP_ENVIRONMENT_CONFIGURATION \
 --env APP_SERVER_PORT=$APP_SERVER_PORT \
 --publish $HOST_PORT:$APP_SERVER_PORT \
