0413ff7bfe89844081ca153513a2e7f2f47cffd7
Richard Snider <richardnsnider@gmail.com>
2021-11-28 09:33:47 -0800

    add code for retrieving s3 file

--- util/aws.go
+++ util/aws.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/aws/aws-sdk-go/service/sts/stsiface"
 )
@@ -19,4 +21,28 @@ func GetAwsIdentity() sts.GetCallerIdentityOutput {
 	}
 
 	return *result
+}
+
+func GetS3File(s3Bucket string, s3Key string) []byte {
+	sess := session.New()
+	var svc s3iface.S3API = s3.New(sess)
+	response, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: &s3Bucket,
+		Key:    &s3Key,
+	})
+
+	if err != nil {
+		Log(err)
+		os.Exit(1)
+	}
+
+	var bodyContent []byte
+	_, readErr := response.Body.Read(bodyContent)
+
+	if readErr != nil {
+		Log(readErr)
+		os.Exit(1)
+	}
+
+	return bodyContent
 }
